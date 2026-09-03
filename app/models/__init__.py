d205a2d9ab03261257a44f1c48e3ca19024ba6d7
ビングー𖦹 <96338235+htsw01102007@users.noreply.github.com>
2026-09-03 07:10:42 +0700

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.models.user import User
-from app.models.campaign import Campaign, CampaignMember
-from app.models.campaign_task import CampaignTask
--- app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.schemas.user import UserCreate, UserResponse, LoginRequest, TokenResponse, RefreshTokenRequest
-from app.schemas.campaign import CampaignCreate, CampaignUpdate, CampaignResponse, MemberAddRequest, MemberResponse
-from app.schemas.campaign_task import TaskCreate, TaskUpdate, TaskResponse, TaskPaginatedResponse
-
